a0cc3d3609f01122b617c15637c8eedd33e028c3
han-pv <pirliyev17@gmail.com>
2025-09-24 19:55:08 +0500

    first commit

---
+++ database/migrations/2025_09_22_150532_create_books_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('author_id')->constrained()->onDelete('cascade');
+            $table->foreignId('language_id')->constrained()->onDelete('cascade');
+            $table->foreignId('publisher_id')->constrained()->onDelete('cascade');
+            $table->foreignId('year_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->float('price');
+            $table->boolean('is_discount')->default(false);
+            $table->integer('discount_percent');
+            $table->string('image')->nullable();
+            $table->string('barcode')->nullable();
+            $table->boolean('is_new')->default(false);
+            $table->boolean('is_stock')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
